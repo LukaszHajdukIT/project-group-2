286eac6be90c7d170cea4d8a3e848cfa337a4375
marcin jasinski <m.jasinskee@gmail.com>
2020-12-11 22:08:19 +0100

    init commit

---
+++ src/main/java/com/example/demo/ProjectGroup2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGroup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGroup2Application.class, args);
+	}
+
+}
