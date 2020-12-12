f9471406dc7117a34f877fe7ada12924a097dc1b
One Visage <onevisage@interia.pl>
2020-12-12 11:49:59 +0100

    added Courses and CoursesRepository

--- src/main/java/com/example/demo/ProjectGroup2Application.java
+++ src/main/java/com/example/demo/ProjectGroup2Application.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.coursers.Courses;
+import com.example.demo.coursers.CoursesRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjectGroup2Application {
@@ -10,4 +14,10 @@ public class ProjectGroup2Application {
 		SpringApplication.run(ProjectGroup2Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner addCourses(CoursesRepository repository){
+		return (args -> {
+			repository.save(new Courses("Java", "Java Basics"));
+		});
+	}
 }
---
+++ src/main/java/com/example/demo/coursers/CoursesRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.coursers;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoursesRepository extends JpaRepository<Courses, Long> {
+
+}
