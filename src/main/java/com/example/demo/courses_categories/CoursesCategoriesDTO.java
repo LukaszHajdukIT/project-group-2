e297d617fc3e8b042041b602b307d282f71481a5
One Visage <onevisage@interia.pl>
2021-01-09 10:23:01 +0100

    add CoursesCategories

---
+++ src/main/java/com/example/demo/courses_categories/CoursesCategoriesDTO.java
@@ -0,0 +1,49 @@
+package com.example.demo.courses_categories;
+
+public class CoursesCategoriesDTO {
+    private Long id;
+    private String name;
+    private String description;
+
+    public CoursesCategoriesDTO() {
+    }
+
+    public CoursesCategoriesDTO(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "CoursesCategories{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
+
---
+++ src/main/java/com/example/demo/courses_categories/CoursesCategoriesRepository.java
@@ -0,0 +1,6 @@
+package com.example.demo.courses_categories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoursesCategoriesRepository extends JpaRepository<CoursesCategories, Long> {
+}
